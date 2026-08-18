c794cd6238bbdc3c83f8438f3a370d64963bb9a5
Aoki Eta <imposter.kore2626@gmail.com>
2026-08-18 18:17:38 +0900

    feat: 引数を追加してファイルを受け取れるようにする。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    // 入力として受け取るファイルパス
+    #[arg(short, long, num_args = 1.., required = true)]
+    input: Vec<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    for path in &args.input {
+        if path.is_file() {
+            println!("{}", path.display())
+        } else {
+            eprintln!("skip: {} (file not found)", path.display())
+        }
+    }
 }
