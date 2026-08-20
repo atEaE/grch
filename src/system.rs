1a16f86110d23623755e1a19752b5e8425309841
Aoki Eta <imposter.kore2626@gmail.com>
2026-08-20 22:50:45 +0900

    feat: game boyを追加

--- src/system.rs
+++ src/system.rs
@@ -2,6 +2,8 @@ use std::path::Path;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum System {
+    /// Game Boy
+    Gb,
     /// Game Boy Advance
     Gba,
     /// Super Family Computer
@@ -12,6 +14,7 @@ impl System {
     // Returns the name of the target system.
     pub fn name(&self) -> &'static str {
         match self {
+            System::Gb => "gb",
             System::Gba => "gba",
             System::Sfc => "sfc",
         }
@@ -21,6 +24,7 @@ impl System {
     pub fn from_path(path: &Path) -> Option<Self> {
         let ext = path.extension().and_then(|e| e.to_str());
         match ext {
+            Some("gb") => Some(System::Gb),
             Some("gba") => Some(System::Gba),
             Some("sfc") => Some(System::Sfc),
             _ => None,
@@ -30,6 +34,7 @@ impl System {
     /// Obtain the DAT file corresponding to the system from https://github.com/libretro/libretro-database .
     pub fn dat_url(&self) -> String {
         match self {
+            System::Gb => "https://raw.githubusercontent.com/libretro/libretro-database/master/metadat/no-intro/Nintendo%20-%20Game%20Boy.dat".to_string(),
 			System::Gba => "https://raw.githubusercontent.com/libretro/libretro-database/master/metadat/no-intro/Nintendo%20-%20Game%20Boy%20Advance.dat".to_string(),
             System::Sfc => "https://raw.githubusercontent.com/libretro/libretro-database/master/metadat/no-intro/Nintendo%20-%20Super%20Nintendo%20Entertainment%20System.dat".to_string(),
 		}
